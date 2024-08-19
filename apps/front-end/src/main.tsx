5e231d05f868cb255953b4682676875a31854fd9
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-08-19 12:48:35 +0100

    main.tsx - initialise Sentry
    Expects VITE_GLITCHTIP_KEY to exist in the `import.meta.env` object,
    which should be obtained from the `.env` file, handled by Vite. The
    VITE_ prefix is required for it to be visible.
    
    This `.env` file is not checked in, purposely.  It should go into a
    password store or somewhere outside the scope of this repository.

--- apps/front-end/src/main.tsx
+++ apps/front-end/src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react"
+import * as Sentry from "@sentry/browser"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import App from "./App"
 import { store } from "./app/store"
 import "./index.css"
+
+// Initialise Sentry SDK ASAP before application load and React initialisation.
+//
+// The key in the DSN needs to be defined in the .env (or .env.*) files, that are loaded
+// by Vite via the dotenv library. https://vitejs.dev/guide/env-and-mode
+Sentry.init({ dsn: `https://${import.meta.env.VITE_GLITCHTIP_KEY}@app.glitchtip.com/7707` })
 
 const container = document.getElementById("root")
 
