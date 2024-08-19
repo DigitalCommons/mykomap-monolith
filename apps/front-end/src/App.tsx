0073801a4f49f3142365e45703da91182573ea19
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-08-19 12:51:16 +0100

    App.tsx - test code to demostrate Glitchtip works

--- apps/front-end/src/App.tsx
+++ apps/front-end/src/App.tsx
@@ -3,11 +3,20 @@ import Map from "./features/map/Map";
 import { Quotes } from "./features/quotes/Quotes";
 import logo from "./logo.svg";
 
+function bang() {
+  console.log("BANG!");
+  throw "bang error";
+  console.log("BANG?");
+  return undefined;
+}
+
 const App = () => {
   return (
+    <>
     <div>
-      <Map />
+      <button onClick={bang}>Break the world</button>
     </div>
+      </>
   );
 };
 
