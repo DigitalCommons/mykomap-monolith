ecdb5e4b68a328d199a2525039b498dda185d607
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-08-19 12:55:12 +0100

    Revert "App.tsx - test code to demostrate Glitchtip works"
    This reverts commit 6214a251ac7d062957dd1b3f378111159675d314.
    
    Keeping it in the codebase to document what I did, but it shouldn't
    remain.

--- apps/front-end/src/App.tsx
+++ apps/front-end/src/App.tsx
@@ -3,20 +3,11 @@ import Map from "./features/map/Map";
 import { Quotes } from "./features/quotes/Quotes";
 import logo from "./logo.svg";
 
-function bang() {
-  console.log("BANG!");
-  throw "bang error";
-  console.log("BANG?");
-  return undefined;
-}
-
 const App = () => {
   return (
-    <>
     <div>
-      <button onClick={bang}>Break the world</button>
+      <Map />
     </div>
-      </>
   );
 };
 
