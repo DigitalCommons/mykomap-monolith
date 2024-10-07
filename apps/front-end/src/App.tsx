a3b05944ec65cbe752eb2c27fd8068cb4e0ec0a1
rogup <rohit5.gup@gmail.com>
2024-10-07 11:58:13 +0100

    [front-end] Remove unused template code (keep Counter for its example of UTs)

--- apps/front-end/src/App.tsx
+++ apps/front-end/src/App.tsx
@@ -1,7 +1,5 @@
-import { Counter } from "./features/counter/Counter";
 import MapWrapper from "./features/map/MapWrapper";
 import SearchBar from "./features/filter/SearchBar";
-import { Quotes } from "./features/quotes/Quotes";
 import logo from "./logo.svg";
 
 const App = () => {
