7938d2e5e3f1727d1407071a7e90c1efb8f0fd47
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-10-09 14:24:22 +0100

    [back-end] src/pluginApi.ts - fixup, handle fastify options correctly
    We need to pass them to the .register() method

--- apps/back-end/src/pluginApi.ts
+++ apps/back-end/src/pluginApi.ts
@@ -19,17 +19,20 @@ const pluginApi: FastifyPluginCallback<MykomapRouterConfig> = async (
   fastify: FastifyInstance,
   opts: MykomapRouterConfig,
 ) => {
+  // Tease apart Mykomap option from all others
+  const { mykomap, ...fastifyOpts } = opts;
+
   // Get ts-rest's machinery going,
   const tsrServer = initServer();
 
   // Feed our implementation to it,
-  const router = tsrServer.router(contract, MykomapRouter(opts));
+  const router = tsrServer.router(contract, MykomapRouter({ mykomap }));
 
   // Plop out a Fastify plugin,
   const plugin = tsrServer.plugin(router);
 
   // And register it.
-  fastify.register(plugin);
+  fastify.register(plugin, fastifyOpts);
 };
 
 /** Export our prepared plugin */
