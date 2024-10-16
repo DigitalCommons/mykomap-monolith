015267d1ac30d678707bd13db3aebf4b6424a0a1
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-10-16 12:45:12 +0100

    [common] contract.ts - move regular expressions into their own file
    i.e. src/rsdefs.ts
    
    We expect to get more in upcoming commits.

--- libs/common/src/api/contract.ts
+++ libs/common/src/api/contract.ts
@@ -1,73 +1,18 @@
 import { initContract } from "@ts-rest/core";
 import { z } from "zod";
 import { extendZodWithOpenApi } from "@anatine/zod-openapi";
+import * as Rx from "../rxdefs.js";
 
 extendZodWithOpenApi(z);
 
 const c = initContract();
 
-/** A regex testing for an *URL-safe* base64 string (RFC4648 sect 5) */
-const UrlSafeBase64Rx = /^[A-Z0-9_-]+$/i;
-
-/** A string regular expression matching an XML NCName
- *
- * - An NCName is an XML Name, but with no colons allowed.
- * - An XML Name is a complicated beast, but loosely a Unicode version of \w,
- *   or in other words, a unicode alphanumeric symbolic identifier.
- *   - It can contain digits, letters, hyphens, periods and underscores and
- *     certain unicode equivalents.
- *   - But it must not start with digits, a hyphen or a period, nor certain
- *     unicode equivalents.
- *
- * The regex for an XML Name is adapted from O'Reilly Regex Cookbook section 8.4,
- * "XML 1.0 names (approximate)" - but the colon is removed.
- *
- * Paraphrasing that book's explanation:
- * - the name start character can be a [:_] or
- * - any of the following Unicode categories:
- *   - Lowercase letter (Ll)
- *   - Uppercase letter (Lu)
- *   - Titlecase letter (Lt)
- *   - Letter without case (Lo)
- *   - Letter number (Nl)
- * - subsequent characters can also include [.-] or
- *   - Mark (M)
- *   - Modifier letter (Lm)
- *   - Decimal digit (Nd)
- *
- * This definition is not compiled, as it is intended for composition below.
- * Therefore it is wrapped in a non-capturing group to isolate it without affecting
- * the captures which may be defined around it.
- *
- * This regex requires node 10+ to be able to use /u and \p.
- *
- */
-const NCName =
-  "(?:[_\\p{Ll}\\p{Lu}\\p{Lt}\\p{Lo}\\p{Nl}][_.\\p{L}\\p{M}\\p{Nd}\\p{Nl}-]*)";
-
-/** Match a QName
- *
- * Paraphrasing https://en.wikipedia.org/wiki/QName
- * - A QName is an NCName (see above)
- * - Or two of them delimited by a colon.
- *
- * Note: for our purposes, we *require* a colon delimiter - it can't just be a NCName.
- * This is because we need an abbreviation with which to look up the URL prefix.
- *
- * FIXME Perhaps we should also disallow common URI scheme prefixes.
- * FIXME Maybe we don't care about unicode?
- *
- * This regex requires node 10+ to be able to use /u and \p.
- *
- */
-const QNameRx = new RegExp(`^${NCName}[:]${NCName}$`, "gsu");
-
 const Location = z.array(z.number()).min(2).max(2);
-const DatasetId = z.string().regex(UrlSafeBase64Rx);
+const DatasetId = z.string().regex(Rx.UrlSafeBase64);
 const DatasetItemId = z.coerce.number().int().nonnegative();
 const DatasetItem = z.object({}).passthrough();
 const Dataset = z.array(Location);
-const QName = z.string().regex(QNameRx);
+const QName = z.string().regex(Rx.QName);
 const VersionInfo = z.object({
   name: z.string(),
   buildTime: z.string().datetime({ offset: false }),
