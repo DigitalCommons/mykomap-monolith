6be978bd54f7531fcd5e8d7125c541a2d3e98d31
Marcel <msouris@gmail.com>
2024-10-21 15:49:50 +0100

    directory item, theme updates, storybook stories

--- apps/front-end/src/components/panel/directoryPanel/directoryItem/DirectoryItem.tsx
+++ apps/front-end/src/components/panel/directoryPanel/directoryItem/DirectoryItem.tsx
@@ -1,13 +1,27 @@
-import React from "react";
 import ListItem from "@mui/material/ListItem";
+import { styled } from "@mui/material/styles";
+import Link from "@mui/material/Link";
+
 interface DirectoryItemProps {
   id: string;
   name: string;
   link: string;
+  onClick?: (e: React.MouseEvent) => void; // for storybook testing
 }
 
-const DirectoryItem = ({id, name, link}: DirectoryItemProps) => {
-  return <ListItem key={id}>{name}</ListItem>;
+const StyledLink = styled(Link)(() => ({
+  color: "var(--color-text)",
+  "&:hover": {
+    backgroundColor: "var(--color-neutral-light)",
+  },
+}));
+
+const DirectoryItem = ({ id, name, link, onClick }: DirectoryItemProps) => {
+  return (
+    <ListItem key={id}>
+      <StyledLink href={link} role="link" onClick={onClick}>{name}</StyledLink>
+    </ListItem>
+  );
 };
 
 export default DirectoryItem;
