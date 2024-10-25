4a754326568161e48eb2a88e618be8d1506d8262
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-10-25 11:22:54 +0100

    [back-end] test/plugin.test.ts - remove API doc comments, refer to contract.ts
    Otheriwse these comments, which are duplicating info elsewhere, will
    likely just become outdated and wrong.

--- apps/back-end/test/plugin.test.ts
+++ apps/back-end/test/plugin.test.ts
@@ -15,38 +15,8 @@ const opts: MykomapRouterConfig = {
   },
 };
 
-//
-// Operation: dataset
-// URL: /dataset/:datasetId/locations
-// summary:  obtains a dataset
-// req.params
-//   type: object
-//   properties:
-//     datasetId:
-//       type: string
-//       description: uniquely specifies the dataset wanted
-//   required:
-//     - datasetId
-//
-// valid responses
-//   '200':
-//     description: the dataset matching the supplied criteria
-//     content:
-//       application/json:
-//         schema:
-//           type: array
-//           items:
-//             maxItems: 2
-//             minItems: 2
-//             type: array
-//             items:
-//               type: number
-//               format: float32
-//   '400':
-//     description: bad input parameter
-//   '404':
-//     description: no such dataset
-//
+// Note: see src/api/contract.ts in the @mykomap/common module for definitions
+// and documentation of the API.
 
 test("testing dataset", async (t) => {
   const fastify = Fastify();
@@ -61,46 +31,6 @@ test("testing dataset", async (t) => {
   expect(res.statusCode).toBe(200);
 });
 
-// Operation: datasetSearch
-// URL: /dataset/:datasetId/search
-// summary:  obtains a list of dataset entries satisfying the search criteria supplied
-// req.params
-//   type: object
-//   properties:
-//     datasetId:
-//       type: string
-//       description: uniquely specifies the dataset wanted
-//   required:
-//     - datasetId
-//
-// req.query
-//   type: object
-//   properties:
-//     text:
-//       type: string
-//       description: a text fragment to match
-//     filter:
-//       type: array
-//       items:
-//         type: string
-//       description: uniquely specifies the taxonomy filter items wanted
-//
-// valid responses
-//   '200':
-//     description: the dataset IDs matching the supplied criteria
-//     content:
-//       application/json:
-//         schema:
-//           type: array
-//           items:
-//             type: string
-//             description: uniquely specifies the dataset item wanted
-//   '400':
-//     description: bad input parameter
-//   '404':
-//     description: no such dataset
-//
-
 test("testing datasetSearch", async (t) => {
   const fastify = Fastify();
   fastify.register(fastifyPlugin, opts);
@@ -113,35 +43,6 @@ test("testing datasetSearch", async (t) => {
   });
   expect(res.statusCode).toBe(200);
 });
-
-// Operation: datasetItem
-// URL: /dataset/:datasetId/item/:datasetItemId
-// summary:  obtains a dataset item by its  unique ID
-// req.params
-//   type: object
-//   properties:
-//     datasetId:
-//       type: string
-//       description: uniquely specifies the dataset wanted
-//     datasetItemId:
-//       type: string
-//       description: uniquely specifies the dataset item wanted
-//   required:
-//     - datasetId
-//     - datasetItemId
-//
-// valid responses
-//   '200':
-//     description: the dataset item matching the supplied criteria
-//     content:
-//       application/json:
-//         schema:
-//           type: object
-//   '400':
-//     description: bad input parameter
-//   '404':
-//     description: no such dataset item
-//
 
 test("testing datasetItem", async (t) => {
   const fastify = Fastify();
