1ab38c09d2d98f2e8e93f80bc33a1a047e99ff6c
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-10-28 12:39:00 +0000

    [back-end] bin/dataset.ts, package.json - the importer script
    Run it with
    
       npm run dataset <parameters here>
    
    Currently we have just one subcommand `import`: reads in a CSV file
    and write the corresponding back-end data to a directory.
    
    The implementation is in `src/dataset.ts` - `bin/dataset.ts` is just
    an executable. This is to allow us to unit test the implementation
    without having to import the executable, which would run the
    implementation immediately in an uncontrolled way.
    
    The CSV data rows are converted to DatasetItem objects. The
    DatasetItem.id property is mandatory, and duplicates are detected.
    
    A `config.json` file needs to be supplied to the script, and this
    should define the properties to put in the data. Also, it defines the
    CSV headers each property should be imported from using the (otherwise
    optional) `from` field. The implementation is fairly simple, and no
    validation is performed yet, apart from requiring a unique ID
    field. The vocabs can also be supplied in this file; this may be used
    to validate data later.

---
+++ apps/back-end/src/dataset.ts
@@ -0,0 +1,263 @@
+import { join } from "node:path";
+import { existsSync } from "node:fs";
+import { open, mkdir, writeFile, FileHandle } from "node:fs/promises";
+import {
+  TextSearch,
+  PropDefs,
+  Dictionary,
+  stringify,
+  toPoint2d,
+  notNullish,
+} from "@mykomap/common";
+import { ValidationError } from "./dataset/csv.js";
+
+/** Information supplied to PropParser functions via `this` */
+export interface PropParserInfo<I, O> {
+  ix: number; // source CSV column index
+  from?: string; // source CSV column header
+  to: string; // destination property name
+  parser: PropParser<I, O>; // the parser function
+}
+
+/** A generic parser for property values.
+ *
+ * @param this: information about the property being parsed
+ * @param v: the value to parse
+ * @returns the parsed result.
+ */
+export type PropParser<I, O> = (this: PropParserInfo<I, O>, v: I) => O;
+
+/** An item from a dataset.
+ *
+ * Essentially this is a dictionary of arbitrary (typically primitive) unknown
+ * values, but there are a few properties which should be
+ * present in all cases.
+ *
+ * Note, multiple values represented by arrays are supported.
+ * Elements of these should be the same type.
+ *
+ * Note: if one of lat or lng properties are absent or undefined,
+ * both should be.
+ */
+export interface DatasetItem {
+  /** A unique identifier within the dataset for this item
+   *
+   * (Not to be confused with its index)
+   */
+  id: string;
+
+  /** An optional name, which may be displayed on the map as a tooltip */
+  name?: string;
+
+  /** A geographical latitude coordinate */
+  lat?: number;
+
+  /** A geographical longitude coordinate */
+  lng?: number;
+
+  /** Everything else! */
+  [key: string]: unknown;
+}
+
+/** Writes datasets to the filesystem in a form usable by the back-end */
+export class DatasetWriter {
+  /** Will be populated with the properties which are searchable */
+  readonly searchedProps: string[] = [];
+
+  /** Will be populated with the properties which are filterable */
+  readonly filteredProps: string[] = [];
+
+  /** Constructs a writer
+   *
+   * @param props - property definitions for the DatasetItems
+   * @param searchProp - the special property of item index objects into which the
+   * "searchable string" - the searchable text index - should be put.
+   */
+  constructor(
+    readonly props: PropDefs,
+    readonly searchProp: string = "searchString",
+  ) {
+    // Identify these searched and filtered properties
+    for (const propName in this.props) {
+      const propDef = this.props[propName];
+
+      if (propDef.search) this.searchedProps.push(propName);
+
+      if (propDef.isFiltered()) this.filteredProps.push(propName);
+    }
+  }
+
+  /** A JSON.stringify replacer for arrays of numbers
+   *
+   * Limits the decimals to 6 DP. This is as much resolution as a world map
+   * with building-level precesion needs.
+   */
+  limitDecimals(_: string, value: any): any {
+    const type = typeof value;
+    switch (type) {
+      case "number":
+        return Number(value.toFixed(6));
+      case "string":
+        return Number(Number(value).toFixed(6));
+      default:
+        return value;
+    }
+  }
+
+  /** A JSON.stringify replacer for objects.
+   *
+   * As above, but only alters  lat/lng properties
+   */
+  limitLatLngDecimals(key: string, value: any): any {
+    if (key === "lat" || key === "lng") {
+      if (value == null) return null; // note: nullish comparison
+      return Number(Number(value).toFixed(6));
+    }
+    return value;
+  }
+
+  /** Write the dataset
+   *
+   * @param string - the path of the directory to create and populate.
+   * It must not pre-exist.
+   * @param id - the ID for the dataset. Used in error messages.
+   * @param items - a stream of DatasetItems to write.
+   */
+  async writeDataset(
+    dirPath: string,
+    id: string,
+    items: AsyncIterable<DatasetItem>,
+  ) {
+    if (existsSync(dirPath))
+      throw new Error(
+        `Cannot create dataset with existing filesystem path: ${dirPath}`,
+      );
+
+    const itemsDir = "items";
+    await mkdir(join(dirPath, itemsDir), { recursive: true });
+
+    let locationFile: FileHandle | undefined;
+    let searchableFile: FileHandle | undefined;
+    const stats: {
+      counter: number;
+      written: number;
+      errors: { message: string; id: string; ix: number }[];
+    } = { counter: 0, written: 0, errors: [] };
+
+    const ids = new Set(); // track duplicates with this
+
+    // Get the list of filterable property names
+    const filterablePropNames = Object.entries(this.props)
+      .map(([name, prop]) => (prop.filter ? name : undefined))
+      .filter(notNullish);
+    filterablePropNames.push(this.searchProp); // This synthetic prop needs to be included
+
+    try {
+      locationFile = await open(join(dirPath, "locations.json"), "w");
+      searchableFile = await open(join(dirPath, "searchable.json"), "w");
+
+      // Write the opening delimiter of the locations
+      locationFile.write("[");
+      // And the opening header of the searchables - indent the object keys so
+      // they stand out as different. The result is intentionally somewhat CSV-like.
+      searchableFile.write(
+        `{   "itemProps":\n` +
+          JSON.stringify(filterablePropNames) +
+          `,\n    "values":[\n`,
+      );
+
+      // Write out each item
+      for await (const item of items) {
+        if (ids.has(item.id)) {
+          stats.errors.push({
+            message: `duplicate ID`,
+            ix: stats.counter++,
+            id: item.id,
+          });
+          continue;
+        }
+        ids.add(item.id);
+
+        if (stats.written !== 0) {
+          // insert delimiters
+          await locationFile.write(",");
+          await searchableFile.write(",\n");
+        }
+
+        // Write the location. Limit the decimals, don't indent.
+        // Bad locations -> null. Note order of coordinates matches
+        // GeoJSON's, i.e. longitude first.
+        // https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.1
+        const point = toPoint2d([item.lng, item.lat], null);
+        const pointJson = JSON.stringify(point, this.limitDecimals);
+        await locationFile.write(pointJson);
+
+        // Write the searchable. No indentation.
+        const searchableItem = this.extractFilterable(item);
+        searchableItem[this.searchProp] = this.textIndex(item);
+
+        const searchableItemString = JSON.stringify(
+          Object.values(searchableItem),
+        );
+        await searchableFile.write(searchableItemString);
+
+        // Write the data item. Indent it this time.
+        // Limit the lat/lng decimals as before.
+        const itemPath = join(
+          dirPath,
+          itemsDir,
+          stats.written.toString() + ".json",
+        );
+        const json = JSON.stringify(item, this.limitLatLngDecimals, "  ");
+        await writeFile(itemPath, json);
+
+        stats.counter += 1;
+        stats.written += 1;
+      }
+
+      // Write the closing delimiter of these JSON files
+      await locationFile.write("]");
+      await searchableFile.write("\n]}");
+    } catch (e) {
+      if (e instanceof ValidationError)
+        throw new Error(`validation error parsing item #${stats.counter}`, {
+          cause: e,
+        });
+
+      throw new Error(
+        `unexpected error creating dataset '${id}' parsing item #${stats.counter} at '${dirPath}'`,
+        { cause: e },
+      );
+    } finally {
+      if (searchableFile) searchableFile.close();
+      if (locationFile) locationFile.close();
+    }
+    return stats;
+  }
+
+  /** Extracts the filterable properties from a DatasetItem to a plain object */
+  extractFilterable(item: DatasetItem) {
+    const filterable: Dictionary<unknown> = {};
+    this.filteredProps.forEach(
+      (propName) => (filterable[propName] = item[propName]),
+    );
+    return filterable;
+  }
+
+  /** Makes a searchable text index value from a DatasetItem */
+  textIndex(item: DatasetItem): string | undefined {
+    // Extract the normalised version of the searchable properties
+    const normText = this.searchedProps.map((propName) => {
+      const propDef = this.props[propName];
+      const value = item[propName];
+
+      const text = propDef.textForValue(value);
+      if (typeof text === "string") return TextSearch.normalise(text);
+      if (text instanceof Array)
+        return text.map((v) => TextSearch.normalise(v)).join(" ");
+      return TextSearch.normalise(stringify(text));
+    });
+
+    return normText.join(" ");
+  }
+}
---
+++ apps/back-end/src/dataset/csv.ts
@@ -0,0 +1,279 @@
+/** Code related to PropSets/PropDefs/DatasetItems and CSV */
+
+import { z, ZodType, ZodTypeDef } from "zod";
+import { Schema } from "udsv";
+import { DatasetItem, PropParser, PropParserInfo } from "../dataset.js";
+import { RowTransformer } from "../csv.js";
+import {
+  InnerValuePropSpec,
+  MultiPropDef,
+  PropDef,
+  PropDefs,
+  VocabDef,
+  VocabPropSpec,
+  boolify,
+  numberify,
+  splitField,
+  stringify,
+} from "@mykomap/common";
+
+/** An error subclass to indicate validation errors */
+export class ValidationError extends Error {
+  constructor(...args: any[]) {
+    super(...args);
+  }
+}
+
+/** Check if a CSV field is considered "empty".
+ *
+ * This currently means:
+ * - undefined
+ * - null
+ * - empty string, ""
+ */
+export function isEmpty(v: unknown): boolean {
+  // Note the deliberate loose comparison with null - includes undefined.
+  return v == null || v === "";
+}
+
+// Wrapper function for zod validations, converting ZodErrors to ValidationErrors
+const validZ = <I, P extends ZodTypeDef, O, Z extends ZodType<O, P, I>>(
+  z: Z,
+  v: unknown,
+): O => {
+  const result = z.safeParse(v);
+  if (result.success) return result.data;
+
+  throw new ValidationError(`validation error`, { cause: result.error });
+};
+
+/** Type of values received by CSV PropParser */
+export type CsvVal = string | null | undefined;
+
+/** Type of values output from CSV PropParser */
+export type SimpleVal = string | number | boolean | undefined | null;
+
+/** Makes a PropDefParser for a CSV field given a PropDef
+ *
+ * Handles the `from`, `as`, `nullable` and `strict` fields, where appropriate
+ */
+export function mkCsvPropDefParser(propDef: PropDef) {
+  // Construct these once at the start.
+  const zString = z.string();
+  const zNumber = z.number();
+  const zBoolean = z.boolean();
+
+  // Parsers for ValuePropSpec
+  function parseValue(
+    propSpec: InnerValuePropSpec,
+  ): PropParser<CsvVal, SimpleVal> {
+    switch (propSpec.nullable) {
+      case true:
+        // Nullable parsers.
+        // Needed as we don't want (for instance) null lat/lng values to become
+        // coerced into zeros.
+
+        // Strict nullable - allow nulls but throw on anything which needs
+        // coersion.
+        if (propSpec.strict)
+          switch (propSpec.as) {
+            case "string":
+              // preserve empty string special case
+              return (v) => (isEmpty(v) ? v : validZ(zString, v));
+            case "number":
+              return (v) => (isEmpty(v) ? null : validZ(zNumber, v));
+            case "boolean":
+              return (v) => (isEmpty(v) ? null : validZ(zBoolean, v));
+            default:
+              return (v) => v;
+          }
+
+        // Sloppy nullable - coerces values to the right type if not null
+        switch (propSpec.as) {
+          case "string":
+            // preserve empty string special case
+            return (v) => (v === "" ? v : stringify(v, null));
+          case "number":
+            return (v) => (isEmpty(v) ? null : numberify(v, null));
+          case "boolean":
+            return (v) => (isEmpty(v) ? null : boolify(v, null));
+          default:
+            return (v) => v;
+        }
+
+      default:
+      case false:
+        switch (propSpec.strict) {
+          case true:
+            // Strict non-nullable parsers
+            switch (propSpec.as) {
+              case "string":
+                return (v) => validZ(zString, v);
+              case "number":
+                return (v) => validZ(zNumber, v);
+              case "boolean":
+                return (v) => validZ(zBoolean, v);
+              default:
+                return (v) => v;
+            }
+
+          default:
+          case false:
+            // Unstrict non-nullable parsers.
+            switch (propSpec.as) {
+              case "string":
+                return (v) => stringify(v, "");
+              case "number":
+                return (v) => numberify(v, 0);
+              case "boolean":
+                return (v) => boolify(v, false);
+              default:
+                return (v) => v;
+            }
+        }
+    }
+  }
+
+  function parseVocab(
+    propDef: VocabPropSpec,
+    vocab: VocabDef,
+  ): PropParser<CsvVal, string | undefined | null> {
+    // FIXME add (non-)nullable and strict semantics later
+    return (v: unknown) => {
+      switch (typeof v) {
+        case "string":
+          if (v in vocab.terms) return v;
+
+          // FIXME this case, an empty-string (so effectively undefined) vocab,
+          // might not be allowed in some cases?
+          // However no way to know with current spec, so just allow it.
+          if (v === "") return null;
+
+          throw new ValidationError(`Invalid ${propDef.uri} vocab URI: '${v}'`);
+
+        default:
+          // FIXME this case, an undefined/null vocab, might not be allowed in some cases?
+          // However no way to know with current spec, so just allow it.
+          if (v === null || v === undefined) return null;
+
+          // Defined, but non-string vocabs are not valid.
+          throw new ValidationError(`Invalid ${propDef.uri} vocab URI: '${v}'`);
+      }
+    };
+  }
+
+  function parseMulti(
+    propDef: MultiPropDef,
+    opts: { delim: string; escape: string },
+  ): PropParser<CsvVal, SimpleVal[]> {
+    // FIXME add (non-)nullable and strict semantics later
+    return (v: unknown) => {
+      switch (typeof v) {
+        case "string":
+          const innerDef = propDef.of;
+          // split, but ignore empty values (always present in degenerate "" case)
+          const values = splitField(v, opts).filter((v) => v != "");
+
+          switch (innerDef.type) {
+            case "value":
+              return values.map(parseValue(innerDef));
+
+            case "vocab":
+              return values.map(parseVocab(innerDef, innerDef.vocab));
+          }
+          throw new Error(`unexpected code path!`); // Belt and braces
+
+        default:
+          // FIXME undefined multis might not be allowed in all cases?
+          // However no way to know with current spec, and it seems
+          // reasonable to interpret them as an empty list.
+          if (v === null || v === undefined) return [];
+
+          throw new ValidationError(`Invalid multi-value property: '${v}'`);
+      }
+    };
+  }
+
+  switch (propDef.type) {
+    case "value":
+      return parseValue(propDef);
+    case "vocab":
+      return parseVocab(propDef, propDef.vocab);
+    case "multi":
+      return parseMulti(propDef, { delim: ";", escape: "\\" });
+  }
+}
+
+/** Constructs a constructor for a parser for transforming CSV rows into
+ * DatasetItems
+ *
+ * The information for doing this arrives at different points, so it
+ * has to be done step by step.
+ * - First, we have the PropDefs
+ * - Then we find out the CSV headers when the CSV is parsed.
+ * - Finally we get a CSV row array and can parse that into a DatasetItem.
+ */
+export function mkCsvParserGenerator(propDefs: PropDefs) {
+  const propParserInfos: Array<PropParserInfo<CsvVal, unknown>> = [];
+
+  // Make sure that there is an ID property at the very least, as DatasetItems
+  // must have that.
+  const idProp = propDefs.id;
+  if (idProp === undefined)
+    throw new Error(`PropDefs must have the mandatory id property defined`);
+  if (idProp.from === undefined)
+    throw new Error(`The PropDef for "id"  must define a 'from' property`);
+
+  // Do as much as we can here - up to but not including the CSV column indexes.
+  for (const key in propDefs) {
+    const propDef = propDefs[key];
+
+    propParserInfos.push({
+      ix: -1,
+      from: propDef.from,
+      to: key,
+      parser: mkCsvPropDefParser(propDef),
+    });
+  }
+
+  // This is the RowTransformer constructor
+  return (schema: Schema): RowTransformer<DatasetItem> => {
+    const headers = schema.cols.map((col) => col.name);
+
+    // Now look up the array correspondances using the headers.
+    // Store them in the propParsers
+    const missing: string[] = [];
+    for (const pp of propParserInfos) {
+      if (pp.from !== undefined) {
+        const ix = headers.indexOf(pp.from);
+        if (ix < 0) missing.push(pp.from);
+        else pp.ix = ix;
+      }
+    }
+
+    // If any headers are missing, that's an error
+    if (missing.length)
+      throw new ValidationError(
+        `CSV has missing headers. Expected: ${JSON.stringify(missing)}`,
+      );
+
+    // Return a DatasetItem transformer
+    return (row: string[]): DatasetItem => {
+      const result: Omit<DatasetItem, "id"> = {};
+
+      for (const ppi of propParserInfos) {
+        try {
+          result[ppi.to] = ppi.parser(row[ppi.ix]);
+        } catch (e) {
+          throw new ValidationError(
+            `whilst parsing prop '${ppi.to}' from CSV field #${ppi.ix + 1}, ` +
+              `'${ppi.from}': ${e instanceof Object && "cause" in e ? e.cause : e}`,
+            { case: e },
+          );
+        }
+      }
+
+      return result as DatasetItem;
+    };
+  };
+}
