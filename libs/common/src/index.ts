64093a1bebd0c01ee0f491a4640e3c56e493fd1a
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2024-11-01 09:26:48 +0000

    [common] src/index.ts - re-export modules with exports
    These can then be accessed outside of this module.

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,4 +1,8 @@
 import specification from "./api/mykomap-openapi.json";
-export { contract, schemas } from "./api/contract.js";
-
 export { specification };
+export * from "./api/contract.js";
+export * from "./utils.js";
+export * from "./common-types.js";
+export * from "./text-search.js";
+export * from "./prop-specs.js";
+export * from "./prop-defs.js";
