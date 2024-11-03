f262ea8fd6d35c0ba73f6e958426ace9ec5d4804
rogup <rohit5.gup@gmail.com>
2024-11-03 22:07:39 +0000

    [front-end] Fetch config in new vocabsSlice instead of searchSlice

--- apps/front-end/src/App.tsx
+++ apps/front-end/src/App.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import MapWrapper from "./components/map/MapWrapper";
 import Panel from "./components/panel/Panel";
 import logo from "./logo.svg";
+import { fetchConfig } from "./app/vocabsSlice";
+import { useAppDispatch } from "./app/hooks";
 
 const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchConfig());
+  }, []);
+
   return (
     <div>
       <MapWrapper />
--- apps/front-end/src/components/panel/searchPanel/SearchPanel.tsx
+++ apps/front-end/src/components/panel/searchPanel/SearchPanel.tsx
@@ -27,10 +27,6 @@ const SearchPanel = () => {
     dispatch(performSearch());
   };
 
-  useEffect(() => {
-    dispatch(fetchConfig());
-  }, []);
-
   return (
     <form className="mx-auto max-w-md" onSubmit={onSubmit}>
       <Heading title="Search">
