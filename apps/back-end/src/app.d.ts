00d585a581e3394f7a30288c28cb097709d966f7
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-03-14 12:33:15 +0000

    [back-end] app.d.ts - declare __BUILD_INFO__ without duplication
    The declaration is needed to allow vite to interpret the
    __BUILD_INFO__ symbol at build time, and insert the value into the
    generated code.

--- apps/back-end/src/app.d.ts
+++ apps/back-end/src/app.d.ts
@@ -1,8 +1,8 @@
-// Tell TS what this is
-declare const __BUILD_INFO__: {
-  name: string;
-  buildTime: string;
-  version: number[];
-  commitDesc: string;
-  nodeEnv: "development"|"production";
-};
+/** __BUILD_INFO__ is used to label builds via the Vite config.
+ *
+ * This tells TS what type it is.
+ *
+ * We use this import to reduce duplication, using a strange style forced on us
+ * by TS.  See https://stackoverflow.com/a/66768386
+ */
+declare const __BUILD_INFO__: import("@mykomap/common").BuildInfo;
