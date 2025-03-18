36ddece3fb8a2042f2dcee3c5bcceb60c39d35e6
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-03-18 17:34:27 +0000

    [common] vite.config.ts - write version and release info into package.json

--- libs/common/vite.config.ts
+++ libs/common/vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { ReadonlyBuildInfo } from "./src/index.js";
+import { spawnSync } from "node:child_process";
 import pkg from "./package.json" with { type: "json" };
 
+// Get the package name and build info
+const __BUILD_INFO__ = new ReadonlyBuildInfo({
+  name: pkg.name,
+  exec: (cmd, args) => spawnSync(cmd, args).stdout.toString(),
+});
+
+// Write the version and sentry release into package.json
+__BUILD_INFO__.updatePackageJson();
+
 export default defineConfig({
+  define: {
+    __BUILD_INFO__,
+  },
   build: {
     lib: {
       entry: "./src/index.ts",
