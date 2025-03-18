a2f783df6bdcf4510d88860ca6c010ffd7875640
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-03-18 17:34:59 +0000

    [node-utils] vite.config.ts - write version and release info into package.json

--- libs/node-utils/vite.config.ts
+++ libs/node-utils/vite.config.ts
@@ -1,12 +1,28 @@
 import { defineConfig } from "vitest/config";
 import dts from "vite-plugin-dts";
 import { fileURLToPath } from "node:url";
+import { ReadonlyBuildInfo } from "@mykomap/common";
+import { spawnSync } from "node:child_process";
+import pkg from "./package.json" with { type: "json" };
 
 // Get the Vite env mode. This should typechecking, but seems not to be?
 //const envMode = envMode: import.meta.env.MODE ?? '';
 
+// Get the package name and build info
+const name = pkg.name;
+const __BUILD_INFO__ = new ReadonlyBuildInfo({
+  name,
+  exec: (cmd: string, args: string[]) => spawnSync(cmd, args).stdout.toString(),
+});
+
+// Write the version and sentry release into package.json
+__BUILD_INFO__.updatePackageJson();
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __BUILD_INFO__,
+  },
   plugins: [dts()],
   build: {
     lib: {
