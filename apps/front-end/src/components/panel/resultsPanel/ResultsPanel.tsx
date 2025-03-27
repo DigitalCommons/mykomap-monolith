85cb0bafe81801aafca7c230008014b18478f625
Marcel <msouris@gmail.com>
2025-03-27 15:09:40 +0000

    refactor ResultsPanel.tsx - remove commented opacity code from slide-in and slide-out animations

--- apps/front-end/src/components/panel/resultsPanel/ResultsPanel.tsx
+++ apps/front-end/src/components/panel/resultsPanel/ResultsPanel.tsx
@@ -22,22 +22,18 @@ import { keyframes } from "@emotion/react";
 const slideIn = keyframes`
   from {
     transform: translateX(-100%);
-    // opacity: 0;
   }
   to {
     transform: translateX(0);
-    // opacity: 1;
   }
 `;
 
 const slideOut = keyframes`
   from {
     transform: translateX(0);
-    // opacity: 1;
   }
   to {
     transform: translateX(-100%);
-    // opacity: 0;
   }
 `;
 
