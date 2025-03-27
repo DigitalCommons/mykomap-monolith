b5a38a9e5210b6472b38b138d551d218fd157b1f
Marcel <msouris@gmail.com>
2025-03-27 09:45:39 +0000

    refactor ResultsPanel.tsx - remove unused state and simplify visibility logic

--- apps/front-end/src/components/panel/resultsPanel/ResultsPanel.tsx
+++ apps/front-end/src/components/panel/resultsPanel/ResultsPanel.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import useMediaQuery from "@mui/material/useMediaQuery";
@@ -44,22 +43,22 @@ const slideOut = keyframes`
 
 const StyledResultsPanel = styled(Drawer)<{
   onClosePanel?: boolean;
-  isClosing?: boolean;
-}>(({ onClosePanel, isClosing }) => ({
+}>(({ onClosePanel }) => ({
   width: "100%",
   height: "100vh",
   position: "relative",
   overflow: "visible",
-  transition: "transform 0.3s ease, opacity 0.3s ease",
+  // transition: "transform 0.3s ease, opacity 0.3s ease",
+  // animation: "slideIn 0.3s ease forwards",
 
   "& .MuiDrawer-paper": {
     width: "100%",
     boxSizing: "border-box",
     backgroundColor: "var(--color-secondary)",
+    visibility: "visible !important",
     overflow: "visible",
     boxShadow: "0 0 20px rgba(0, 0, 0, 0.16)",
     animation: `${onClosePanel ? slideOut : slideIn} 0.3s ease forwards`,
-    visibility: isClosing ? "hidden" : "visible",
   },
 
   "@media (min-width: 897px)": {
@@ -91,22 +90,12 @@ const ResultsPanel = () => {
   const panelOpen = useAppSelector(selectPanelOpen);
   const resultsPanelOpen = useAppSelector(selectResultsPanelOpen);
 
-  const [isVisible, setIsVisible] = useState(false);
-  const [isClosing, setIsClosing] = useState(false);
-
-  const animationDuration = 300;
+  console.log("resultsPanelOpen", resultsPanelOpen);
 
   const isMedium = useMediaQuery("(min-width: 897px)");
 
   const handleToggle = () => {
-    if (isMedium && panelOpen && resultsPanelOpen) {
-      dispatch(closeResultsPanel());
-      setTimeout(() => {
-        dispatch(togglePanel());
-      }, animationDuration);
-    } else {
       dispatch(togglePanel());
-    }
   };
 
   const handlePanelClose = () => {
@@ -119,53 +108,43 @@ const ResultsPanel = () => {
   const handleClearSearch = () => {
     console.log("Clear search");
     if (!isMedium) dispatch(setSelectedTab(0));
-    // dispatch(closePanel());
     dispatch(closeResultsPanel());
     dispatch(clearSearch());
   };
 
-  useEffect(() => {
-    if (panelOpen && resultsPanelOpen) {
-      setIsVisible(true);
-    } else {
-      const timeout = setTimeout(() => {
-        setIsVisible(false);
-        setIsClosing(false);
-      }, animationDuration);
-      return () => clearTimeout(timeout);
-    }
-  }, [panelOpen, resultsPanelOpen]);
-
   return (
     <>
-      {isVisible && (
-        <StyledResultsPanel
-          open={panelOpen && resultsPanelOpen}
-          onClosePanel={!(panelOpen && resultsPanelOpen)}
-          isClosing={isClosing}
-          variant="persistent"
-          anchor="left"
-        >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              height: "100%",
-            }}
+      {panelOpen &&
+        resultsPanelOpen && ( // Adding this stops the drawer transition working
+          <StyledResultsPanel
+            open={panelOpen && resultsPanelOpen}
+            onClosePanel={!(panelOpen && resultsPanelOpen)}
+            variant="persistent"
+            anchor="left"
           >
-            <StyledButtonContainer>
-              <StandardButton buttonAction={handleClearSearch}>
-                {t("clear_search")}
-              </StandardButton>
-              {!isMedium && <CloseButton buttonAction={handlePanelClose} />}
-            </StyledButtonContainer>
-            <Results />
-          </Box>
-          {isMedium && resultsPanelOpen && (
-            <PanelToggleButton buttonAction={handleToggle} isOpen={panelOpen} />
-          )}
-        </StyledResultsPanel>
-      )}
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                height: "100%",
+              }}
+            >
+              <StyledButtonContainer>
+                <StandardButton buttonAction={handleClearSearch}>
+                  {t("clear_search")}
+                </StandardButton>
+                {!isMedium && <CloseButton buttonAction={handlePanelClose} />}
+              </StyledButtonContainer>
+              <Results />
+            </Box>
+            {isMedium && (
+              <PanelToggleButton
+                buttonAction={handleToggle}
+                isOpen={panelOpen}
+              />
+            )}
+          </StyledResultsPanel>
+        )}
     </>
   );
 };
