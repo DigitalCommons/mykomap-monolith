689a6ce36cffc7f5bdd9a9eedce50422111c966a
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-06-10 18:08:55 +0100

    [front-end] components/map/{markers,makLibre}.ts - use correct marker order
    Problem:
    - the marker icon order doesn't match the order of the vocab, but
      should
    - they use vocab IDs which add extra hardwiring of Powys concepts into the code
    
    Resolution:
    - Put them into the correct order
    - Use generic IDs, since these don't seem to matter
    - We can also now simplify the object mapping them into an array

--- apps/front-end/src/components/map/markers.ts
+++ apps/front-end/src/components/map/markers.ts
@@ -4,14 +4,18 @@ import emergencyFoodMarker from "./markers/emergency-food-marker.png";
 import eventsMarker from "./markers/events-marker.png";
 import localFoodMarker from "./markers/local-food-marker.png";
 
-//when adding new markers, ensure default is still the last entry
-//these labels are hardwired and this is just a temporary situation, to be rectified later
-const markers = {
-  "fs:cg": communityGrowingMarker,
-  "fs:ef": emergencyFoodMarker,
-  "fs:ev": eventsMarker,
-  "fs:lm": localFoodMarker,
-  "default-marker": defaultMarker,
-};
+//when adding new markers, ensure default is still the last entry these labels
+//are hardwired and this is just a temporary situation, to be rectified later
+//
+// The order should match the definition of terms in the vocab property named by
+// config.json's ui.marker_property_name value. There should be one extra icon
+// for cases which take multple values, if these cases exist.
+const markers = [
+  emergencyFoodMarker, // CAT10
+  communityGrowingMarker, // CAT20
+  localFoodMarker, // CAT30
+  eventsMarker, // CAT40
+  // No multi-category case anticipated
+];
 
 export default markers;
