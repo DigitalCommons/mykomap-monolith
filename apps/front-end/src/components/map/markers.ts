a809557ebb0cac2d7698172efa83bbaab079a0ff
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-06-11 18:15:34 +0100

    [front-end] components/map/markers.ts - fixup marker mapping
    Problem: food_system_category is a vocab with 19 terms, some of which
    are top-level terms which have markers. But if only 5 mappings are
    supplied, this doesn't supply a mapping for all those terms, nor even
    a vaguely correct one.
    
    Resolution: We need to supply mappings for all of the terms, not just
    the top-level ones. (Or we need to split the vocab into two vocabs,
    and deal with the resulting complication, currently hoping to avoid
    that)

--- apps/front-end/src/components/map/markers.ts
+++ apps/front-end/src/components/map/markers.ts
@@ -11,11 +11,25 @@ import localFoodMarker from "./markers/local-food-marker.png";
 // config.json's ui.marker_property_name value. There should be one extra icon
 // for cases which take multple values, if these cases exist.
 const markers = [
-  emergencyFoodMarker, // CAT10
-  communityGrowingMarker, // CAT20
-  localFoodMarker, // CAT30
-  eventsMarker, // CAT40
-  // No multi-category case anticipated
+  emergencyFoodMarker, // "ep": "Emergency food provision",
+  emergencyFoodMarker, // "ep-fb": " - Food bank",
+  emergencyFoodMarker, // "ep-cf": " - Community fridge",
+  emergencyFoodMarker, // "ep-pm": " - Pay as you feel meals",
+  emergencyFoodMarker, // "ep-cf": " - Community food share",
+  communityGrowingMarker, // "cg": "Community growing",
+  communityGrowingMarker, // "cg-ie": " - Incredible Edible",
+  communityGrowingMarker, // "cg-ga": " - Community garden",
+  communityGrowingMarker, // "cg-or": " - Community orchard",
+  communityGrowingMarker, // "cg-al": " - Allotments",
+  localFoodMarker, // "lp": "Local food provider",
+  localFoodMarker, // "lp-gr": " - Farmer/grower",
+  localFoodMarker, // "lp-cr": " - Cafe/restaurant",
+  localFoodMarker, // "lp-mk": " - Market",
+  localFoodMarker, // "lp-sp": " - Shop",
+  eventsMarker, // "ev": "Events",
+  eventsMarker, // "ev-ff": " - Food festival",
+  eventsMarker, // "ev-cc": " - Cooking classes"
+  defaultMarker, // No multi-category case anticipated
 ];
 
 export default markers;
