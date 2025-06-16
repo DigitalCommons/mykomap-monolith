63d594f0ff4d16a5da0385f34a3d1f2807ab89ce
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2025-06-16 13:33:04 +0100

    [back-end] cli/dataset.ts - encapsulate DatasetWriter construction for marker case
    Move the code for building a DatasetWriter for the case where there is
    a marker_property_name setting into a helper method, to encapsulate it
    for clarity.  Add some extra comments.
    
    Essentuially a cosmectic change - the functionality should not be
    changed by this.

--- apps/back-end/src/cli/dataset.ts
+++ apps/back-end/src/cli/dataset.ts
@@ -1,5 +1,5 @@
 import { Command, Option, UsageError } from "clipanion";
-import { ConfigData, VocabIndex, schemas } from "@mykomap/common";
+import { ConfigData, PropDefs, VocabIndex, schemas } from "@mykomap/common";
 import { slurpJson } from "@mykomap/node-utils";
 import { zodCheck } from "./clipanion-interop.js";
 import { DatasetItem, DatasetWriter } from "../dataset.js";
@@ -79,105 +79,29 @@ export class ImportCmd extends Command {
     const langs = config.languages;
 
     // A PropDefsFactory instance using our vocab definitions
-    const pds = new PropDefsFactory(vocabs, langs);
+    const pdf = new PropDefsFactory(vocabs, langs);
 
     const propSpecs: PropSpecs = config.itemProps; // Types from contract should be compatible
-    const propDefs = pds.mkPropDefs(propSpecs);
+    const propDefs = pdf.mkPropDefs(propSpecs);
 
     const mkTransformer = mkCsvParserGenerator(propDefs);
     const csvReader = fromCsvFile(this.csvPath, mkTransformer);
 
     try {
       const markerName = config.ui.marker_property_name;
-      let dsWriter: DatasetWriter;
 
-      if (markerName !== undefined) {
-        const markerPropDef = propDefs[markerName];
+      // Construct an appropriate DatasetWriter for our case
+      let dsWriter: DatasetWriter =
+        markerName === undefined
+          ? new DatasetWriter(propDefs)
+          : this._mkDatasetWriterWithMarkerIcons(
+              propSpecs,
+              propDefs,
+              config,
+              markerName,
+            );
 
-        if (markerPropDef == undefined)
-          throw new Error(
-            `No item property called '${markerName}' found in the config definitions`,
-          );
-        if (markerPropDef.uri == undefined)
-          throw new Error(
-            `The item property '${markerName}' is not a vocab property:\n` +
-              JSON.stringify(propSpecs[markerName]),
-          );
-
-        this.context.stdout.write(
-          `Using the item property '${markerName}' to infer marker type to use,\n` +
-            `which has the vocab '${markerPropDef.uri}'.\nFull specification:\n` +
-            JSON.stringify(propSpecs[markerName]) +
-            "\n\n",
-        );
-
-        // Validate markerPropDef.uri
-        const ncname = parseAbbrevUri(markerPropDef.uri, null);
-        if (ncname == null)
-          throw new Error(
-            `The marker property '${markerName}' does not have a valid ` +
-              `URI abbreviation: ` +
-              markerPropDef.uri,
-          );
-
-        if (!(ncname in config.vocabs))
-          throw new Error(
-            `The marker property '${markerName}' does not reference a known ` +
-              `vocab URI: ` +
-              markerPropDef.uri,
-          );
-
-        const vocab = config.vocabs[ncname];
-        const lang = Object.keys(vocab)[0];
-        const termIndex = vocab[lang].terms || {}; // be defensive!
-        const terms = Object.keys(termIndex);
-        this.context.stdout.write(
-          "Vocab terms are mapped to icon indexes as follows:\n" +
-            terms
-              .map((term, ix) => ` - #${ix}: ${term}\t"${termIndex[term]}"`)
-              .join("\n") +
-            "\n\n",
-        );
-
-        // Extend DatasetWriter with an appropriate markerIndex method which
-        // looks up a marker index from the selected item property, if set, in
-        // the vocab terms.
-        //
-        // Returns one of:
-        // - undefined (no value or an empty list),
-        // - the index of a vocab term (a single matching value)
-        // - the number of vocab terms (if a multiple valued list)
-        // - minus one (if a single unmatching value)
-        dsWriter = new (class extends DatasetWriter {
-          override markerIndex(item: DatasetItem): number | undefined {
-            let value = item[markerName];
-
-            // Check there's a value present. Note the loose inequality,
-            // matches null or undefined.
-            if (value == undefined) return undefined;
-
-            if (markerPropDef.type !== "multi") {
-              // A singlar value
-              return terms.indexOf(String(value));
-            }
-
-            // Must be a list of values
-            const values = value as Array<string>;
-            switch (values.length) {
-              case 0:
-                return undefined; // No values
-              case 1:
-                return terms.indexOf(String(values[1])); // Single valued array
-              default:
-                return terms.length; // Multiple value array
-            }
-          }
-        })(propDefs);
-      }
-
-      // If not set already, use this default FIXME detect errors?
-      dsWriter ??= new DatasetWriter(propDefs);
-
+      // Write out the dataset
       const stats = await dsWriter.writeDataset(this.dataPath, "id", csvReader);
       await cp(this.configPath, join(this.dataPath, "config.json"));
       this.context.stdout.write(
@@ -190,5 +114,112 @@ export class ImportCmd extends Command {
       console.debug(e);
       throw new UsageError(reason);
     }
+  }
+
+  /** Helper method to construct a DatasetWriter instance for the case with marker icons.
+   *
+   * Also reports on the validation process to stdout, therefore specific to the usage here
+   * in the Clipanion implementation.
+   *
+   * @param propSpecs: A PropSpecs collection. Used only for reports.
+   * @param propDefs: A PropDefs corresponding to propSpecs. Used for construction.
+   * @param markerName: The name of the PropDef to use to select marker icons for items. To
+   * be validated before use.
+   * @returns a new DatasetWriter if everything validates ok.
+   */
+  protected _mkDatasetWriterWithMarkerIcons(
+    propSpecs: PropSpecs,
+    propDefs: PropDefs,
+    config: ConfigData,
+    markerName: string,
+  ): DatasetWriter {
+    // If we get here, we have a marker property name configuration to interpret.
+    // Get the associated property definition, if one exists, and validate it.
+    const markerPropDef = propDefs[markerName];
+
+    // Verify that markerPropDef exists and it's a property concerning a
+    // vocab (singular or multi valued).
+    if (markerPropDef == undefined)
+      throw new Error(
+        `No item property called '${markerName}' found in the config definitions`,
+      );
+    if (markerPropDef.uri == undefined)
+      throw new Error(
+        `The item property '${markerName}' is not a vocab property:\n` +
+          JSON.stringify(propSpecs[markerName]),
+      );
+
+    // Now validated, report the situation on the console.
+    this.context.stdout.write(
+      `Using the item property '${markerName}' to infer marker type to use,\n` +
+        `which has the vocab '${markerPropDef.uri}'.\nFull specification:\n` +
+        JSON.stringify(propSpecs[markerName]) +
+        "\n\n",
+    );
+
+    // Validate that markerPropDef.uri is one we know from the config
+    const ncname = parseAbbrevUri(markerPropDef.uri, null);
+    if (ncname == null)
+      throw new Error(
+        `The marker property '${markerName}' does not have a valid ` +
+          `URI abbreviation: ` +
+          markerPropDef.uri,
+      );
+    if (!(ncname in config.vocabs))
+      throw new Error(
+        `The marker property '${markerName}' does not reference a known ` +
+          `vocab URI: ` +
+          markerPropDef.uri,
+      );
+
+    // Now get the list of terms associated to that vocab
+    const vocab = config.vocabs[ncname];
+    const lang = Object.keys(vocab)[0];
+    const termIndex = vocab[lang].terms || {}; // be defensive!
+    const terms = Object.keys(termIndex);
+
+    // Report the list of terms in use.
+    this.context.stdout.write(
+      "Vocab terms are mapped to icon indexes as follows:\n" +
+        terms
+          .map((term, ix) => ` - #${ix}: ${term}\t"${termIndex[term]}"`)
+          .join("\n") +
+        "\n\n",
+    );
+
+    // Extend DatasetWriter with an appropriate markerIndex method which
+    // looks up a marker index from the selected item property, if set, in
+    // the vocab terms.
+    //
+    // Returns one of:
+    // - undefined (no value or an empty list),
+    // - the index of a vocab term (a single matching value)
+    // - the number of vocab terms (if a multiple valued list)
+    // - minus one (if a single unmatching value)
+    return new (class extends DatasetWriter {
+      override markerIndex(item: DatasetItem): number | undefined {
+        let value = item[markerName];
+
+        // Check there's a value present. Note the loose inequality,
+        // matches null or undefined.
+        if (value == undefined) return undefined;
+
+        if (markerPropDef.type !== "multi") {
+          // A singlar value
+          return terms.indexOf(String(value));
+        }
+
+        // Must be a list of values
+        const values = value as Array<string>;
+        switch (values.length) {
+          case 0:
+            return undefined; // No values
+          case 1:
+            return terms.indexOf(String(values[1])); // Single valued array
+          default:
+            return terms.length; // Multiple value array
+        }
+      }
+    })(propDefs);
   }
 }
