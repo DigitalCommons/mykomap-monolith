7769733533357eff1f1d34bad55166d6f88b3609
John Evans <john.software.evans@gmail.com>
2025-08-08 19:26:29 +0100

    [BE] - create functions to get items by id, split off existing by ix

--- apps/back-end/src/routes.ts
+++ apps/back-end/src/routes.ts
@@ -7,6 +7,8 @@ import {
   getDatasetConfig,
   getDatasetItemByIx,
   getDatasetItemById,
+  getDatasetItemByIx,
+  getDatasetItemById,
   getDatasetLocations,
   getTotals,
   initDatasets,
@@ -101,7 +103,7 @@ export function MykomapRouter(
       const itemId = itemIdOrIx;
       const item = getDatasetItemById(datasetId, itemId);
 
-      return { status: 200, body: item };
+      return { status: 200, body: { ...item } };
     },
 
     getTotals: async ({ params: { datasetId } }) => {
