265bc05fd21b385d34490243de4ebf4563e3bfc1
Marcel <msouris@gmail.com>
2025-12-09 18:44:12 +0000

    [front-end] Add DotCoop key to results

---
+++ apps/front-end/src/components/panel/resultsPanel/results/DataSourceKey.tsx
@@ -0,0 +1,66 @@
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import CheckIcon from "@mui/icons-material/Check";
+import theme from "../../../../theme/theme";
+
+interface DataSourceKeyProps {
+  results: { data_sources?: string[] }[];
+}
+
+const StyledKeyContainer = styled("div")(({ theme }) => ({
+  width: "fit-content",
+  display: "flex",
+  alignItems: "center",
+  marginTop: "10px",
+  fontSize: "var(--font-size-small)",
+  fontWeight: "var(--font-weight-normal)",
+  color: "#fff",
+  textTransform: "uppercase",
+  border: `2px solid ${theme.palette.secondary.light}`,
+  backgroundColor: theme.palette.secondary.light,
+  letterSpacing: "0.1em",
+  "& div": {
+    display: "flex",
+    alignItems: "center",
+    marginLeft: "5px",
+    padding: "3px 6px 3px 2px",
+  },
+}));
+
+const DataSourceKey = ({ results }: DataSourceKeyProps) => {
+  const hasDCSource = results.some((item) => item.data_sources?.includes("DC"));
+
+  if (!hasDCSource) {
+    return null;
+  }
+
+  // Add DotCoop logo to the key if DC source is present
+
+  return (
+    <StyledKeyContainer>
+      <CheckIcon
+        style={{
+          display: "flex",
+          color: "var(--color-dc-source)",
+          backgroundColor: theme.palette.secondary.main,
+          alignSelf: "center",
+        }}
+      />{" "}
+      <div>
+        <img
+          src="/assets/logos/dotcoop-logo-small.png"
+          alt="DotCoop Logo"
+          style={{
+            margin: "3px 5px 0 0",
+            
+            height: "17px",
+            width: "auto",
+          }}
+        />
+        Verified
+      </div>
+    </StyledKeyContainer>
+  );
+};
+
+export default DataSourceKey;
