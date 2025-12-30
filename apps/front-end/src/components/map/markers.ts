f93d9b97e3b08b9e122bafb4cfb5a45c9509ad03
Marcel <msouris@gmail.com>
2025-12-30 15:12:48 +0000

    [front-end] consolidate DotCoop and Powys markers

--- apps/front-end/src/components/map/markers.ts
+++ apps/front-end/src/components/map/markers.ts
@@ -1,5 +1,9 @@
 import defaultMarker from "./markers/map-marker.png";
 import dotCoopMarker from "./markers/dotcoop-marker.png";
+// import communityGrowingMarker from "./markers/community-growing-marker.png";
+// import emergencyFoodMarker from "./markers/emergency-food-marker.png";
+// import eventsMarker from "./markers/events-marker.png";
+// import localFoodMarker from "./markers/local-food-marker.png";
 
 // When adding new markers, ensure default is still the last entry. FIXME These labels
 // are hardwired and this is just a temporary situation, to be rectified later.
@@ -10,7 +14,7 @@ import dotCoopMarker from "./markers/dotcoop-marker.png";
 // config.json's ui.marker_property_name value. There should be one extra icon
 // for cases which take multple values, if these cases exist.
 //
-// For dec-2025-data dataset: uses data_sources (ods vocab) for markers
+// For CWM dataset: uses data_sources (ods vocab) for markers
 // Index mapping: 0=NCBA, 1=DC, 2=ICA, 3=USDA, 4=NCG, 5=FICU, 6=FCA, 7=CUK,
 // 8=USFWC, 9=CMC, 10=IFFCO, 11=ICS, 12=ACMEI, 13=WC, 14=NCUI, 15=COOPERAR,
 // 16=EUROCOOP, 17=YCC, 18=Multiple values
@@ -33,8 +37,28 @@ const markers = [
   defaultMarker, // 15: COOPERAR
   defaultMarker, // 16: EUROCOOP
   defaultMarker, // 17: YCC
-  defaultMarker, // 18: Multiple values (currently shows default, not dotCoop)
+  defaultMarker, // 18: Multiple values without DC (multi-values with DC show dotCoop at index 1)
   defaultMarker, // 19: Default marker (must be last)
+  // Powys variant markers:
+  // emergencyFoodMarker, // "ep": "Emergency food provision",
+  // emergencyFoodMarker, // "ep-fb": " - Food bank",
+  // emergencyFoodMarker, // "ep-cf": " - Community fridge",
+  // emergencyFoodMarker, // "ep-pm": " - Pay as you feel meals",
+  // emergencyFoodMarker, // "ep-cf": " - Community food share",
+  // communityGrowingMarker, // "cg": "Community growing",
+  // communityGrowingMarker, // "cg-ie": " - Incredible Edible",
+  // communityGrowingMarker, // "cg-ga": " - Community garden",
+  // communityGrowingMarker, // "cg-or": " - Community orchard",
+  // communityGrowingMarker, // "cg-al": " - Allotments",
+  // localFoodMarker, // "lp": "Local food provider",
+  // localFoodMarker, // "lp-gr": " - Farmer/grower",
+  // localFoodMarker, // "lp-cr": " - Cafe/restaurant",
+  // localFoodMarker, // "lp-mk": " - Market",
+  // localFoodMarker, // "lp-sp": " - Shop",
+  // eventsMarker, // "ev": "Events",
+  // eventsMarker, // "ev-ff": " - Food festival",
+  // eventsMarker, // "ev-cc": " - Cooking classes"
+  // defaultMarker, // No multi-category case anticipated
 ];
 
 export default markers;
