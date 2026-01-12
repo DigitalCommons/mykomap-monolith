381fe54bda11ca0d0dcaaeb360301f0c24059b41
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2026-01-12 15:52:43 +0000

    [back-end]  services/datasetService.ts - catch dataset load errors
    ...and allow the load of other datasets to proceed.
    
    See https://github.com/DigitalCommons/mykomap-monolith/issues/233

--- apps/back-end/src/services/datasetService.ts
+++ apps/back-end/src/services/datasetService.ts
@@ -26,9 +26,21 @@ export const initDatasets = (dataRoot: string) => {
 
   console.log("Found datasets:", datasetIds);
 
+  const loadedDatasetIds: string[] = [];
   for (const datasetId of datasetIds) {
-    datasets[datasetId] = new Dataset(datasetId, dataRoot);
+    try {
+      datasets[datasetId] = new Dataset(datasetId, dataRoot);
+      loadedDatasetIds.push(datasetId);
+    } catch (e) {
+      // Report the short form error at error level
+      console.error(`Dataset '${datasetId}' failed to load, skipping it: ${e}`);
+      // Report the long-form error cause at debug level
+      if (e instanceof Error && e.cause)
+        console.debug(`Dataset '${datasetId}' failed because: ${e.cause}`);
+    }
   }
+
+  console.log("Loaded datasets:", loadedDatasetIds);
 };
 
 const getDatasetOrThrow404 = (
@@ -42,12 +54,18 @@ const getDatasetOrThrow404 = (
   return dataset;
 };
 
-export const getDatasetItemByIx = (datasetId: string, datasetItemIx: number) => {
+export const getDatasetItemByIx = (
+  datasetId: string,
+  datasetItemIx: number,
+) => {
   const dataset = getDatasetOrThrow404(contract.getDatasetItem, datasetId);
   return dataset.getItemByIx(datasetItemIx);
 };
 
-export const getDatasetItemById = (datasetId: string, datasetItemId: string) => {
+export const getDatasetItemById = (
+  datasetId: string,
+  datasetItemId: string,
+) => {
   const dataset = getDatasetOrThrow404(contract.getDatasetItem, datasetId);
   return dataset.getItemById(datasetItemId);
 };
