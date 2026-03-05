347a628e39522ac4c8e7d85d867f32feb8164876
Marcel <msouris@gmail.com>
2026-03-05 15:01:54 +0000

    [front-end] added dotcoop marker

--- apps/front-end/src/components/map/markers.ts
+++ apps/front-end/src/components/map/markers.ts
@@ -3,7 +3,7 @@ import communityGrowingMarker from "./markers/community-growing-marker.png";
 import emergencyFoodMarker from "./markers/emergency-food-marker.png";
 import eventsMarker from "./markers/events-marker.png";
 import localFoodMarker from "./markers/local-food-marker.png";
-import dotcoopMarker from "./markers/map-marker.png";
+import dotcoopMarker from "./markers/dotcoop-marker.png";
 
 const allMarkers: { [key: string]: string } = {
   "emergencyFoodMarker": emergencyFoodMarker,
